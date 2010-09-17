8118a0bdb95dc32e9fa0a386a9f40f243e98dedf
Warren Brian Noronha <warren.noronha@gmail.com>
2010-09-17 06:19:18 +0530

    Demo app on Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceAuthDemo::Application.config.secret_token = '8d1cba9c181953d9d0660c95a7e2532e5145fa3620b3ba5525ea0f4b12b80a7194b8bf594b9cc34ff3be904480bbf72f612f2bc1e6430e29d5399ab8baed0c92'
